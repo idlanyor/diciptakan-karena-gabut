079f9882bed43bcf84397604705b41a979e9aae9
roynaldi3301 <prasbhara0604@gmail.com>
2024-01-18 19:22:31 +0700

    lah

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,7 @@
 const ENDPOINT_PPDB = "https://ppdbapi.rosevelt.my.id/api/";
 // const ENDPOINT_PPDB = "http://localhost:8000/api/"
+// const ENDPOINT_PPDB = "http://localhost/backend/public/api/"
 // const ENDPOINT_PPDB = "http://192.168.160.118/backend-2/public/api/"
-// export const STORAGE = "http://localhost:8000/storage/"
-export const STORAGE = "https://ppdbapi.rosevelt.my.id/storage/"
+// export const STORAGE = "http://localhost:8000/storage/filependaftar/"
+export const STORAGE = "https://ppdbapi.rosevelt.my.id/storage/filependaftar/"
 export default ENDPOINT_PPDB;
