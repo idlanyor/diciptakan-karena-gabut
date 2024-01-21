74d69b0c73c91581749d6083de8ef329279f4987
roynaldi3301 <prasbhara0604@gmail.com>
2024-01-22 03:43:52 +0700

    hehe

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-const ENDPOINT_PPDB = "https://backend.roidev.my.id//api/";
+const ENDPOINT_PPDB = "https://backend.roidev.my.id/api/";
 // const ENDPOINT_PPDB = "http://localhost:8000/api/"
 // const ENDPOINT_PPDB = "http://localhost/backend/public/api/"
 // const ENDPOINT_PPDB = "http://192.168.160.118/backend-2/public/api/"
