223b671b57ca5ae88aab0b94f2ec9579625d4cd0
roynaldi3301 <prasbhara0604@gmail.com>
2024-01-22 04:06:33 +0700

    update new endpoint

--- src/constants.js
+++ src/constants.js
@@ -3,5 +3,5 @@ const ENDPOINT_PPDB = "https://backend.roidev.my.id/api/";
 // const ENDPOINT_PPDB = "http://localhost/backend/public/api/"
 // const ENDPOINT_PPDB = "http://192.168.160.118/backend-2/public/api/"
 // export const STORAGE = "http://localhost:8000/storage/filependaftar/"
-export const STORAGE = "https://backend.roidev.my.id/storage/filependaftar/"
+export const STORAGE = "https://ppdb.storage.roidev.my.id/"
 export default ENDPOINT_PPDB;
